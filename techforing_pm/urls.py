45e611cb8f8e937f57ac571ab25fdf77caf629b9
bhawish44 <130421291+bhawish44@users.noreply.github.com>
2025-05-16 20:29:35 +0500

    new things

--- techforing_pm/urls.py
+++ techforing_pm/urls.py
@@ -22,8 +22,13 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-   openapi.Info(title="Project Management API", default_version='v1'),
+   openapi.Info(
+      title="Project Management API",
+      default_version='v1',
+      description="API docs for TechForing Project Manager",
+   ),
    public=True,
+#    permission_classes=(permissions.AllowAny,),
 )
 
 router = DefaultRouter()
